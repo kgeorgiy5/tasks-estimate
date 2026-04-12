43710451855a6192ee6746e9ce7777dfaa7f441c
kgeorgiy5 <kulikovskiy.g.05@gmail.com>
2026-04-12 21:36:40 +0300

    feat: Modified user modal to reference current task

--- packages/server/src/modules/users/models/user.model.ts
+++ packages/server/src/modules/users/models/user.model.ts
@@ -1,13 +1,16 @@
-import { Schema, Document } from "mongoose";
+import { Schema, Document, Types, SchemaTypes } from "mongoose";
+import { TASK_MODEL_TOKEN } from "../../tasks/models/task.model";
 
 export const USER_MODEL_TOKEN = "User";
 
 export interface User extends Document {
   email: string;
   password: string;
+  currentTaskId?: Types.ObjectId;
 }
 
 export const UserSchema = new Schema<User>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  currentTaskId: { type: SchemaTypes.ObjectId, ref: TASK_MODEL_TOKEN },
 });
